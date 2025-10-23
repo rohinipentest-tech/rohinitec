1dd220b382b387454b074f65196aae9944011ee8
unknown <rohinicyberarc@gmail.com>
2025-10-23 12:19:15 +0530

    first commit

---
+++ hidden.py
@@ -0,0 +1,13 @@
+
+# #list hidden files suspicious executable files
+
+# import os
+
+# for file in os.listdir("."):
+#     if file.startswith(".") or file.endswith((".exe", ".bat", ".sh")):
+#         print("Suspicious File:",file)
+
+
+
+
+
